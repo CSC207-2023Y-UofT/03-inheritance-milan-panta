33e726f0d3481af5237ec283ce33de9528af82c9
milan-panta <keyvalue2002@gmail.com>
2023-05-19 16:24:44 -0400

    Bag implementation with String array

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -5,6 +5,8 @@
  * 1. Introduction to Java helpful.
  */
 
+import javax.lang.model.type.UnionType;
+
 public abstract class Bag {
     /*
      * TODO: Create the following private instance variables
@@ -13,8 +15,10 @@ public abstract class Bag {
      *       - an int named capacity
      *       - an array of Strings named contents
      */
-
-
+    private String color;
+    private int numberOfContents;
+    private int capacity;
+    private String[] contents;
 
 
     /*
@@ -26,8 +30,12 @@ public abstract class Bag {
      * be empty (e.g. numberOfContents is 0 and an empty String array for
      * its contents.)
      */
-
-
+    public Bag(String color, int capacity) {
+        this.color = color;
+        this.capacity = capacity;
+        this.numberOfContents = 0;
+        this.contents = new String[capacity];
+    }
 
 
     /*
@@ -37,17 +45,26 @@ public abstract class Bag {
      *           - getNumberOfContents
      *           - getCapacity
      */
+    public String getColor() {
+        return this.color;
+    }
 
+    public int getNumberOfContents() {
+        return this.numberOfContents;
+    }
 
+    public int getCapacity() {
+        return this.capacity;
+    }
 
 
     /*
      * TODO: Create a setter function called setColor which sets the
      *       color of this bag to the given color.
      */
-
-
-
+    public void setColor(String new_color) {
+        this.color = new_color;
+    }
 
 
     /*
@@ -60,9 +77,12 @@ public abstract class Bag {
      *       This method should return true if the item was added
      *       and false otherwise.
      */
-
-
-
+    public void addItem(String new_item) {
+        if (this.numberOfContents < this.capacity) {
+            this.contents[this.numberOfContents] = new_item;
+            this.numberOfContents++;
+        }
+    }
 
 
     /**
@@ -75,11 +95,12 @@ public abstract class Bag {
      *
      * @return
      */
-
-
-
-
-
+    public String popItem() {
+        if (this.numberOfContents > 0) {
+            this.contents[numberOfContents - 1];
+            this.numberOfContents--;
+        }
+    }
     /**
      * Increase this bag's capacity by n.
      *
